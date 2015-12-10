ca7c1af0421fb65ed122d97f8d30a6a742fa8321
Nathan Luo <naipton@gmail.com>
2015-12-10 17:09:11 -0500

    Add event controller and event partial view

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,4 +1,13 @@
 class EventsController < ApplicationController
   def index
+    @events = Event.all
+  end
+
+  def show
+  end
+
+  def near
+    events = Event.near(params[:latlng_bounds]);
+    render json: events.to_json
   end
 end
