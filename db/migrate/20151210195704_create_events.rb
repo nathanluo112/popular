825883aec98222ea9957f47d369b04b5e86f3958
Salim Rahimi <email": null>
2015-12-10 15:38:40 -0500

    Fix migration misspelling

--- db/migrate/20151210195704_create_events.rb
+++ db/migrate/20151210195704_create_events.rb
@@ -2,7 +2,7 @@ class CreateEvents < ActiveRecord::Migration
   def change
     create_table :events do |t|
       t.string :latlng, null: false
-      t.sttring :address
+      t.string :address
       t.string :venue_name, null: false
       t.integer :final_score, null: false
       t.timestamps null: false
