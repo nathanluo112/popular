b3c7489b57a41ff6274838805127f717b7925515
Ian Lockwood <email": null>
2015-12-11 17:30:37 -0500

    Add methods for updating Event Score and doling out popularity points.
    Check my algebra!

--- db/seeds.rb
+++ db/seeds.rb
@@ -9,6 +9,7 @@ Event.create(lat: 40.71766400377538, lng: -74.00004386901855, venue_name: "Aplac
 Event.create(lat: 40.715387077160194, lng: -74.00579452514648, venue_name: "Bplace", score: 0)
 Event.create(lat: 40.72449431636692, lng: -73.9793586730957, venue_name: "Cplace", score: 0)
 
+<<<<<<< 14b81f6ce5f19c79fc12dd53f1c22e4708068aa4
 User.create(popularity: 5)
 User.create(popularity: 10)
 User.create(popularity: 15)
@@ -20,3 +21,35 @@ Vote.create(user_id: 3, votable: Event.last, vote_direction: -1)
 
 Remark.create(description: "Hey hair is wonderful", giver_id: 1, receiver_id: 2, event: Event.last, remark_direction: 1)
 Remark.create(description: "boots are terrible bruh",giver_id: 3, receiver_id: 1, event: Event.last, remark_direction: -1)
+=======
+
+User.create(popularity: 100)
+User.create(popularity: 100)
+User.create(popularity: 100)
+User.create(popularity: 100)
+User.create(popularity: 100)
+User.create(popularity: 100)
+User.create(popularity: 100)
+User.create(popularity: 100)
+User.create(popularity: 100)
+User.create(popularity: 100)
+User.create(popularity: 100)
+User.create(popularity: 100)
+User.create(popularity: 100)
+User.create(popularity: 100)
+
+Vote.create(user_id: 1, event_id: 1, vote_direction: 1)
+Vote.create(user_id: 2, event_id: 1, vote_direction: 1)
+Vote.create(user_id: 3, event_id: 1, vote_direction: 1)
+Vote.create(user_id: 4, event_id: 1, vote_direction: 1)
+Vote.create(user_id: 5, event_id: 1, vote_direction: -1)
+Vote.create(user_id: 6, event_id: 1, vote_direction: -1)
+Vote.create(user_id: 7, event_id: 1, vote_direction: 1)
+Vote.create(user_id: 8, event_id: 1, vote_direction: 1)
+Vote.create(user_id: 9, event_id: 1, vote_direction: 1)
+Vote.create(user_id: 10, event_id: 1, vote_direction: 1)
+Vote.create(user_id: 11, event_id: 1, vote_direction: 1)
+Vote.create(user_id: 12, event_id: 1, vote_direction: 1)
+Vote.create(user_id: 13, event_id: 1, vote_direction: 1)
+Vote.create(user_id: 14, event_id: 1, vote_direction: 1)
+>>>>>>> Add methods for updating Event Score and doling out popularity points.
