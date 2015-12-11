0c377a45c48acf0d72623763bbafb7e2b95f21e7
mamelend <m.melend@gmail.com>
2015-12-10 19:43:14 -0500

    Add functionality for votes in controller and view

--- db/schema.rb
+++ db/schema.rb
@@ -17,12 +17,12 @@ ActiveRecord::Schema.define(version: 20151210195704) do
   enable_extension "plpgsql"
 
   create_table "events", force: :cascade do |t|
-    t.string   "latlng",      null: false
+    t.string   "latlng",     null: false
     t.string   "address"
-    t.string   "venue_name",  null: false
-    t.integer  "final_score", null: false
-    t.datetime "created_at",  null: false
-    t.datetime "updated_at",  null: false
+    t.string   "venue_name", null: false
+    t.integer  "score",      null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "users", force: :cascade do |t|
