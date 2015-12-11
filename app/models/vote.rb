ab8c2a32283d05acfc53170746989a04558ba260
Ian Lockwood <email": null>
2015-12-11 18:15:59 -0500

    Account for vote polymorphism

--- app/models/vote.rb
+++ app/models/vote.rb
@@ -1,16 +1,13 @@
 class Vote < ActiveRecord::Base
   belongs_to :user
   belongs_to :votable, polymorphic: true
-  # belongs_to :event
   validates_uniqueness_of :user, scope: :votable
-  belongs_to :event
-  validates_uniqueness_of :user, scope: :event
 
-  before_save :add_popularity_to_event
+  before_save :add_popularity_to_votable
 
   private
-  def add_popularity_to_event
-    self.event.score += (self.user.popularity * self.vote_direction)
-    self.event.save
+  def add_popularity_to_votable
+    self.votable.score += (self.user.popularity * self.vote_direction)
+    self.votable.save
   end
 end
