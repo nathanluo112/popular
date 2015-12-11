8ccec150c50768fc79f77f09666fda00452946b6
Ian Lockwood <email": null>
2015-12-10 19:40:56 -0500

    Add location descriptions in map

--- app/assets/javascripts/map.js
+++ app/assets/javascripts/map.js
@@ -1,69 +1,69 @@
-$(document).ready(function(){
-  var map;
-  // var markers = [];
+// $(document).ready(function(){
+//   var map;
+//   // var markers = [];
 
-  var coordinates = function() {
-    window.navigator.geolocation.getCurrentPosition(function(args){
-      coords = {
-        lat: args['coords']['latitude'],
-        lng: args['coords']['longitude']
-      };
-      initMap(coords);
-    });
-  };
+//   var coordinates = function() {
+//     window.navigator.geolocation.getCurrentPosition(function(args){
+//       coords = {
+//         lat: args['coords']['latitude'],
+//         lng: args['coords']['longitude']
+//       };
+//       initMap(coords);
+//     });
+//   };
 
-  function initMap(coordinates) {
-  // Create a map object and specify the DOM element for display.
-    map = new google.maps.Map(document.getElementById('map'), {
-      center: coordinates,
-      scrollwheel: false,
-      zoom: 13
-    });
+//   function initMap(coordinates) {
+//   // Create a map object and specify the DOM element for display.
+//     map = new google.maps.Map(document.getElementById('map'), {
+//       center: coordinates,
+//       scrollwheel: false,
+//       zoom: 13
+//     });
 
-    var marker = new google.maps.Marker({
-      position: coordinates,
-      map: map,
-      title: 'Your location'
-    });
-    nearBy();
-  };
+//     var marker = new google.maps.Marker({
+//       position: coordinates,
+//       map: map,
+//       title: 'Your location'
+//     });
+//     nearBy();
+//   };
 
-  function nearBy(){
-    var request = {
-      location: new google.maps.LatLng(coords),
-      radius: 4000,
-      types: ['bar','night_club','stadium']
-    };
+//   function nearBy(){
+//     var request = {
+//       location: new google.maps.LatLng(coords),
+//       radius: 4000,
+//       types: ['bar','night_club','stadium']
+//     };
 
-    var callback = function(results, status) {
-      // debugger
-      if (status == google.maps.places.PlacesServiceStatus.OK) {
-        for (var i = 0; i < results.length; i++) {
-          var place = results[i];
-          console.log(place.name);
-          createMarker(results[i]);
-        }
-      }
-    }
-    // debugger
-    service = new google.maps.places.PlacesService(map)
-    service.nearbySearch(request, callback);
+//     var callback = function(results, status) {
+//       // debugger
+//       if (status == google.maps.places.PlacesServiceStatus.OK) {
+//         for (var i = 0; i < results.length; i++) {
+//           var place = results[i];
+//           console.log(place.name);
+//           createMarker(results[i]);
+//         }
+//       }
+//     }
+//     // debugger
+//     service = new google.maps.places.PlacesService(map)
+//     service.nearbySearch(request, callback);
 
-  }
+//   }
 
-function createMarker(place) {
-  var placeLoc = place.geometry.location;
-  var marker = new google.maps.Marker({
-    map: map,
-    position: place.geometry.location
-  });
+// function createMarker(place) {
+//   var placeLoc = place.geometry.location;
+//   var marker = new google.maps.Marker({
+//     map: map,
+//     position: place.geometry.location
+//   });
 
-  google.maps.event.addListener(marker, 'click', function() {
-    infowindow.setContent(place.name);
-    infowindow.open(map, this);
-  });
-}
+//   google.maps.event.addListener(marker, 'click', function() {
+//     infowindow.setContent(place.name);
+//     infowindow.open(map, this);
+//   });
+// }
 
-  coordinates();
+//   coordinates();
 
-});
+// });
