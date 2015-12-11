5f829fcbfd52eed2267e44aff7f4ae96e95a743c
mamelend <m.melend@gmail.com>
2015-12-10 20:07:03 -0500

    quick fix to js votes

--- app/assets/javascripts/votes.js
+++ app/assets/javascripts/votes.js
@@ -1,5 +1,4 @@
-// Place all the behaviors and hooks related to the matching controller here.
-// All this logic will automatically be available in application.js.
+
 $(document).ready(function(){
   $("#vote").on("click", "form", function(event){
     event.preventDefault();
