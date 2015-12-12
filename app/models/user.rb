fc5f32f2a003c23b97534e394f62c21290a69f39
mamelend <m.melend@gmail.com>
2015-12-12 18:48:36 -0500

    Fix remark associations, you can't love/hate yourself

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,4 +5,9 @@ class User < ActiveRecord::Base
   def voted_for?(event)
     Vote.where(user: self, votable: event).count > 0
   end
+
+  def remarked_on?(receiver)
+    Remark.where(giver_id: self.id, receiver_id: receiver.id) > 0
+  end
+
 end
