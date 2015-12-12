fe569ccfa1c50d4cf557c37b61311f83f55102b1
Salim Rahimi <email": null>
2015-12-12 18:25:50 -0500

    A

--- app/assets/javascripts/user_show.js
+++ app/assets/javascripts/user_show.js
@@ -1,8 +1,9 @@
 var app = angular.module("user-show", []);
 
 app.controller('user-controller', function ($scope, $http) {
-  $http.get('phones');
+  $http.get(<%= user_path %>).then();
   $scope.user = s;
+
 
 
 
