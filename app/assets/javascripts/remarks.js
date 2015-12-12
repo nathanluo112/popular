892f8fd5fea79be9e4547931e8a6e29e50d08841
mamelend <m.melend@gmail.com>
2015-12-12 17:40:04 -0500

    love and hate forms are seperated for events show

--- app/assets/javascripts/remarks.js
+++ app/assets/javascripts/remarks.js
@@ -7,19 +7,57 @@ $(document).ready(function(){
   //   debugger
   // });
 
-  $(".remark_buttons").on("click", "a", function(event){
+  // $(".remark_buttons").on("click", "a", function(event){
+  //   event.preventDefault();
+    
+  //   $(event.target).parent().hide();
+  //   $(event.target).parent().next().show();
+  // });
+
+  $(".remark_buttons").on("click", "a.hate", function(event){
     event.preventDefault();
+    
+    $(event.target).parent().hide();
+    $(event.target).parent().next().next().show();
+  });
+
+    $(".remark_buttons").on("click", "a.love", function(event){
+    event.preventDefault();
+    
     $(event.target).parent().hide();
     $(event.target).parent().next().show();
   });
 
-  $("[name = 'back']").on("click", function(event){
+  $(".love_form").on("click", "button", function(event){
     event.preventDefault();
-    $(event.target).parent().hide();
+        
+    $(event.target).parent().parent().hide();
     $(event.target).parent().parent().prev().show();
   });
 
-  $(".remark_form").on("submit", "form", function(event){
+  $(".hate_form").on("click", "button" ,function(event){
+    event.preventDefault();
+    
+    $(event.target).parent().parent().hide();
+    $(event.target).parent().parent().prev().prev().show();
+  });
+
+  $(".love_form").on("submit", "form", function(event){
+    event.preventDefault();
+    $.ajax({
+      method: "post",
+      url:    $(event.target).attr("action"),
+      data:   $(event.target).serialize()
+    }).done(function(response){ 
+       
+      $(event.target).parent().hide();
+      $(event.target).parent().next().css("visibility", "visible");
+    }).fail(function(error){
+      console.log("You've already made a remark.");
+    });
+  });
+
+  $(".hate_form").on("submit", "form", function(event){
     event.preventDefault();
     $.ajax({
       method: "post",
