b62974e2e6e49010527db176b5675f035f48d5d4
Ian Lockwood <email": null>
2015-12-13 16:08:13 -0500

    Add colors and sizes to markers based on popularity of event
    Right now, these are static numbers.
    That is okay for now, but it would not work in a professional rollout - if I live in a small town, my event will never be considered bumpin.
    Later, I will write an algorithm that queries for all current events in a city and grabs an array of their popularity, and then splits them up into fifths.

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,9 +8,15 @@
 default_pic = "default_face.png"
 
 Event.create(lat: 40.71766400377538, lng: -74.00004386901855, venue_name: "Aplace", score: 0)
-Event.create(lat: 40.715387077160194, lng: -74.00579452514648, venue_name: "Bplace", score: 0)
-Event.create(lat: 40.72449431636692, lng: -73.9793586730957, venue_name: "Cplace", score: 0)
-Event.create(lat: 40.706299941436384, lng: -74.0090936422348, venue_name: "DBC", score: 0)
+Event.create(lat: 40.715387077160194, lng: -74.00579452514648, venue_name: "Bplace", score: 11)
+Event.create(lat: 40.72449431636692, lng: -73.9793586730957, venue_name: "Cplace", score: 101)
+Event.create(lat: 40.706299941436384, lng: -74.0090936422348, venue_name: "DBC", score: 501)
+Event.create(lat: 40.72966400377538, lng: -74.00504386901855, venue_name: "hello", score: 1001)
+Event.create(lat: 40.716387077160194, lng: -74.00579452514648, venue_name: "i", score: 1200)
+Event.create(lat: 40.72049431636692, lng: -73.9723586730957, venue_name: "am", score: 700)
+Event.create(lat: 40.708299941436384, lng: -74.0060936422348, venue_name: "ian", score: 15)
+
+
 User.create(first_name: "bob", popularity: 5, profile_pic_url: default_pic)
 User.create(first_name: "tob", popularity: 10, profile_pic_url: default_pic)
 User.create(first_name: "rob", popularity: 15, profile_pic_url: default_pic)
