92535d04562a67beed081db63fa321e85b1c59fa
srahimi1 <watid2005@aim.com>
2015-12-13 11:16:57 -0500

    User show page slideup divs

--- app/assets/javascripts/user_show.js
+++ app/assets/javascripts/user_show.js
@@ -1,9 +1,18 @@
 
 var app=angular.module("usershow", [])
 app.controller('usercontroller', function($scope, $http) {
-     $("#user_events").hide();
-     // $("#user_remarks_made").hide();
-     // $("#user_remarks_received").hide();
+     $(".switch").click(function(e){
+        for (var x=0; x < 3; x++) {
+          $($(".accordian")[x]).slideUp();
+        }
+       $(this).next().slideDown(); 
+     });
+
+     for (var x=1; x < 3; x++) {
+          $($(".accordian")[x]).slideUp();
+        }
+
+
      $http.get("/current_user_data").then(function(res){
       console.log(res.data);
       $scope.user = res.data;
