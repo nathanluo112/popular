df8e95d15120afc357f15066cb65ffdaccdc9933
mamelend <m.melend@gmail.com>
2015-12-14 16:53:37 -0500

    ajax on remarks

--- app/controllers/remarks_controller.rb
+++ app/controllers/remarks_controller.rb
@@ -3,7 +3,7 @@ class RemarksController < ApplicationController
   def create
     event = Event.find_by(id: params[:event_id])
     remark = Remark.new(permit_params.merge(giver_id: current_user.id, receiver_id: params[:remark][:user_id], event_id: params[:event_id]))
-    
+    binding.pry
     if remark.save
       
       redirect_to event_path(event)
