e181dd12e095187d75bc731503874f8424fbc2e9
mamelend <m.melend@gmail.com>
2015-12-15 17:31:53 -0500

    Add swipe functionality for event show page

--- app/assets/javascripts/swipe.js
+++ app/assets/javascripts/swipe.js
@@ -1,14 +1,13 @@
-$(document).ready(function(){
+// $(document).ready(function(){
 
-  $(function() {
-    $("#swipe").swipe( {
-      //Generic swipe handler for all directions
-      swipeDown:function(event, direction, distance, duration, fingerCount, fingerData) {
-        // location.reload();
-        console.log("you swiped " + direction );
-        // $("#reload")   
-      }
-    });
-  });
+//   $(function() {
+//     $("#swipe").swipe( {
+//       //Generic swipe handler for all directions
+//       swipeDown:function(event, direction, distance, duration, fingerCount, fingerData) {
+//         // location.reload();
+//         console.log("you swiped " + direction );
+//       }
+//     });
+//   });
 
-});
+// });
