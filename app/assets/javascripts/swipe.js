71fcd93596f0be02326139cb73eb621fbc56c40e
mamelend <m.melend@gmail.com>
2015-12-16 14:11:38 -0500

    swipe testing

--- app/assets/javascripts/swipe.js
+++ app/assets/javascripts/swipe.js
@@ -3,14 +3,14 @@ $(document).ready(function(){
   $(function() {
     $("#event_show").swipe({
       
-      swipe:function(event, direction, distance, duration, fingerCount, fingerData) {
+      swipeRight:function(event, direction, distance, duration, fingerCount, fingerData) {
         console.log("you swiped" + direction);
         $(".name").toggle();
         $(".address").toggle();
         $(".description").toggle();
       },
       
-      swipe:function(event, direction, distance, duration, fingerCount, fingerData) {
+      swipeLeft:function(event, direction, distance, duration, fingerCount, fingerData) {
         console.log("you swiped" + direction);
         $(".name").toggle();
         $(".address").toggle();
