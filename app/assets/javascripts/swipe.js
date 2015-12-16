6287b131cf86366d108ffb511e90cadc25ecba6b
mamelend <m.melend@gmail.com>
2015-12-16 13:28:26 -0500

    another swipe fix

--- app/assets/javascripts/swipe.js
+++ app/assets/javascripts/swipe.js
@@ -1,14 +1,15 @@
 $(document).ready(function(){
 
   $(function() {
-    $("#event_show").swipe( {
+    $("#event_show").swipe({
       
       swipe:function(event, direction, distance, duration, fingerCount, fingerData) {
         console.log("you swiped" + direction);
         $(".name").toggle();
         $(".address").toggle();
         $(".description").toggle();
-      }
+      },
+      
       swipe:function(event, direction, distance, duration, fingerCount, fingerData) {
         console.log("you swiped" + direction);
         $(".name").toggle();
