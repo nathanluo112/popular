ef01bdd6d23327d6bf523ef7ab45a5771d432a65
Nathan Luo <nathanluo@Nathans-MacBook-Air.local>
2016-02-04 13:47:03 -0500

    Set up environment

--- app/assets/javascripts/fb.js
+++ app/assets/javascripts/fb.js
@@ -1,43 +1,43 @@
-  var fb_login_after;
-  // This is called with the results from from FB.getLoginStatus().
-  function statusChangeCallback(response) {
-    // The response object is returned with a status field that lets the
-    // app know the current login status of the person.
-    // Full docs on the response object can be found in the documentation
-    // for FB.getLoginStatus().
-    if (response.status === 'connected') {
-      $("#connect-directions").hide();
-      // Logged into your app and Facebook.
-    } else if (response.status === 'not_authorized') {
-      // The person is logged into Facebook, but not your app.
-      $.get("/logout", function(res){ res=res+""; if (res=="reload") window.location.reload(true);}); //window.location.reload(true);
-      document.getElementById('fb-status').innerHTML = 'Please log ' +
-        'into this app.';
-    } else {
-      $.get("/logout", function(res){ res=res+""; if (res=="reload") window.location.reload(true);});
-      $("#connect-directions").show();
-      document.getElementById('fb-status').innerHTML = 'Please log ' +
-        'into Facebook.';
-    }
+var fb_login_after;
+// This is called with the results from from FB.getLoginStatus().
+function statusChangeCallback(response) {
+  // The response object is returned with a status field that lets the
+  // app know the current login status of the person.
+  // Full docs on the response object can be found in the documentation
+  // for FB.getLoginStatus().
+  if (response.status === 'connected') {
+    $("#connect-directions").hide();
+    // Logged into your app and Facebook.
+  } else if (response.status === 'not_authorized') {
+    // The person is logged into Facebook, but not your app.
+    $.get("/logout", function(res){ res=res+""; if (res=="reload") window.location.reload(true);}); //window.location.reload(true);
+    document.getElementById('fb-status').innerHTML = 'Please log ' +
+    'into this app.';
+  } else {
+    $.get("/logout", function(res){ res=res+""; if (res=="reload") window.location.reload(true);});
+    $("#connect-directions").show();
+    document.getElementById('fb-status').innerHTML = 'Please log ' +
+    'into Facebook.';
   }
+}
 
-  // This function is called when someone finishes with the Login
-  // Button.  See the onlogin handler attached to it in the sample
-  // code below.
-  function checkLoginState() {
-    FB.getLoginStatus(function(response) {
-      statusChangeCallback(response);
-    });
-  }
+// This function is called when someone finishes with the Login
+// Button.  See the onlogin handler attached to it in the sample
+// code below.
+function checkLoginState() {
+  FB.getLoginStatus(function(response) {
+    statusChangeCallback(response);
+  });
+}
 
-  window.fbAsyncInit = function() {
+window.fbAsyncInit = function() {
   FB.init({
-    appId      : '1692192954346001',
+    appId: '1325363560823152',
 
-    cookie     : true,  // enable cookies to allow the server to access
-                        // the session
-     xfbml      : true,
-     version    : 'v2.5'
+    cookie: true,  // enable cookies to allow the server to access
+                      // the session
+    xfbml      : true,
+    version    : 'v2.5'
   });
 
   $(window).focus(function(){
@@ -49,33 +49,29 @@
     statusChangeCallback(response);
   });
 
-FB.Event.subscribe('auth.logout', logout_event);
+  FB.Event.subscribe('auth.logout', logout_event);
   function logout_event(){$.get("/logout", function(res) {res=res+""; if (res=="reload") window.location.reload(true);})}
 
 
   fb_login_after = function($el) {
-  FB.api('/me',{fields: 'last_name,first_name,gender,id'}, function(response1){
-    FB.api("/me/picture", {type: "large" }, function(response2) {if (response2 && !response2.error) {
-    $.post("/users", {first_name: response1.first_name,last_name: response1.last_name, facebook_id: response1.id, profile_pic_url: response2.data.url}, function(res) {$("#fade-out-div").fadeOut(600);
-      $.get("/events").then(function(res){
-
-        $('body').fadeOut(400, function(){
-         document.location.href = "/events";
-        //   $('body').fadeIn(400, function(){
-        //     // document.location.href = "/events"
-        // });
-        });
-
-
-
-       }   ); });
-    //window.location.reload(true);
-    } });
-    $('#fb-status').innerHTML = 'Thanks for logging in, ' + response1.name + '!';
+    FB.api('/me',{fields: 'last_name,first_name,gender,id'}, function(response1){
+      FB.api("/me/picture", {type: "large" }, function(response2) {
+        if (response2 && !response2.error) {
+          $.post("/users", {first_name: response1.first_name,last_name: response1.last_name, facebook_id: response1.id, profile_pic_url: response2.data.url}, function(res) {
+            $("#fade-out-div").fadeOut(600);
+            $.get("/events").then(function(res){
+              $('body').fadeOut(400, function(){
+                document.location.href = "/events";
+              });
+            }); 
+          });
+        }
+      });
+      $('#fb-status').innerHTML = 'Thanks for logging in, ' + response1.name + '!';
     });
-} //  end function fb_login_after() {
+  }
 
-  };
+};
 
   // Load the SDK asynchronously
   (function(d, s, id) {
